738ca14e83c5dfdaa4aa5be9d38f3bdc93ee3e06
kazushi <kazu0226_7382@yahoo.co.jp>
2019-10-17 08:25:14 +0000

    finish ch07

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'   #=>contact_path
   get  '/signup', to: 'users#new'
+  resources :users
  
 end
